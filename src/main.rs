d7480f8a855174b76c105b5eae0f7e15265f2938
grimtin10 <grimtin10@gmail.com>
2024-06-19 16:10:34 -0600

    implement basic tokenization

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,17 @@ use tokenizer::tokenize;
 mod tokenizer;
 
 fn main() {
+    let start = std::time::Instant::now();
     let file_path = "helloworld.rasm";
 
     let contents = fs::read_to_string(file_path).expect("failed to read file");
     let lines = contents.split("\n");
 
-    
     for line in lines {
-        let tokens = tokenize(line);
-        println!("{}", line)
+        if line.trim().len() > 0 {
+            let tokens = tokenize(line.to_string());
+            println!("{:?}", tokens)
+        }
     }
+    eprintln!("assembly took {:.2}ms", start.elapsed().as_secs_f32() * 1000f32);
 }
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,18 +1,108 @@
-enum Token {
-    NUMBER,
-    TYPE,
+#[derive(Debug)]
+pub enum Type {
+
+}
+
+#[derive(Debug)]
+pub enum Token {
+    NUMBER(u128),
+    TYPE(Type),
     LPAREN,
     RPAREN,
     LCURLY,
     RCURLY,
     LSQUARE,
     RSQUARE,
-    QUOTE,
+    STRING(String),
     DOT,
     COMMA,
-    IDENT,
+    IDENT(String),
 }
 
-pub fn tokenize(line: &str) -> Vec<Token> {
-    
+macro_rules! push {
+    ($tokens:expr, $cur_token:expr) => {
+        if $cur_token.len() > 0 {
+            $tokens.push(Token::IDENT($cur_token));
+        }
+    };
+}
+
+pub fn tokenize(line: String) -> Vec<Token> {
+    let mut tokens: Vec<Token> = Vec::new();
+
+    let mut cur_token: String = String::from("");
+    let mut in_str = false;
+    for c in line.chars() {
+        if !in_str {
+            match c {
+                '(' => {
+                    push!(tokens, cur_token);
+                    cur_token = String::from("");
+                    tokens.push(Token::LPAREN);
+                }
+                ')' => {
+                    push!(tokens, cur_token);
+                    cur_token = String::from("");
+                    tokens.push(Token::RPAREN);
+                }
+                '{' => {
+                    push!(tokens, cur_token);
+                    cur_token = String::from("");
+                    tokens.push(Token::LCURLY);
+                }
+                '}' => {
+                    push!(tokens, cur_token);
+                    cur_token = String::from("");
+                    tokens.push(Token::RCURLY);
+                }
+                '[' => {
+                    push!(tokens, cur_token);
+                    cur_token = String::from("");
+                    tokens.push(Token::LSQUARE);
+                }
+                ']' => {
+                    push!(tokens, cur_token);
+                    cur_token = String::from("");
+                    tokens.push(Token::RSQUARE);
+                }
+                '"' => {
+                    in_str = true;
+                }
+                '.' => {
+                    push!(tokens, cur_token);
+                    cur_token = String::from("");
+                    tokens.push(Token::DOT);
+                }
+                ',' => {
+                    push!(tokens, cur_token);
+                    cur_token = String::from("");
+                    tokens.push(Token::COMMA);
+                }
+                ' ' => {
+                    if cur_token.len() > 0 {
+                        tokens.push(Token::IDENT(cur_token));
+                        cur_token = String::from("");
+                    }
+                }
+                '\r' | '\n' => {
+                    if cur_token.len() > 0 {
+                        tokens.push(Token::IDENT(cur_token));
+                        cur_token = String::from("");
+                    }
+                }
+                _ => cur_token.push(c)
+            }
+        } else {
+            // yes im using match for this
+            match c {
+                '"' => {
+                    tokens.push(Token::STRING(cur_token));
+                    cur_token = String::from("");
+                }
+                _ => cur_token.push(c)
+            }
+        }
+    }
+
+    return tokens;
 }
